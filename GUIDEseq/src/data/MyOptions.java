0b0f9ad5877fa698d081fa7a379947c7778f620e
rvanschendel <R.van_Schendel@lumc.nl>
2019-05-06 14:17:59 +0200

    added command line options

---
+++ GUIDEseq/src/data/MyOptions.java
@@ -0,0 +1,103 @@
+package data;
+
+import java.io.File;
+import java.lang.reflect.Method;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.ParseException;
+
+public class MyOptions {
+private CommandLine cmd;
+	
+	public MyOptions(CommandLine cmd) {
+		this.cmd = cmd;
+	}
+
+	public File getBam() {
+		String fileName = cmd.getOptionValue("input");
+		File f = new File(fileName);
+		if(!f.exists()) {
+			System.err.println("Bam file "+f.getAbsolutePath()+" does not exist");
+			System.exit(0);
+		}
+		return f;
+	}
+
+	public File getRefFile() {
+		String fileName = cmd.getOptionValue("reference");
+		File f = new File(fileName);
+		if(!f.exists()) {
+			System.err.println("Reference file "+f.getAbsolutePath()+" does not exist");
+			System.exit(0);
+		}
+		return f;
+	}
+	public String getChr() {
+		return cmd.getOptionValue("chr");
+	}
+	public boolean isFirstOfPairFlag() {
+		if(cmd.hasOption("P5")) {
+			return true;
+		}
+		if(cmd.hasOption("P7")) {
+			return false;
+		}
+		System.err.println("Either P5 or P7 has to be specified");
+		System.exit(0);
+		return false;
+	}
+
+	public String getPrimer() {
+		return cmd.getOptionValue("primer").toUpperCase();
+	}
+	public String getPrimerPart(int number) {
+		String primer =  cmd.getOptionValue("primer").toUpperCase();
+		if(primer.length()>number) {
+			return primer.substring(0, number);
+		}
+		return primer;
+	}
+
+	public long getMinSupport() {
+		if(cmd.hasOption("minsupport")) {
+			try {
+				return (Long) cmd.getParsedOptionValue("minsupport");
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return 5;
+	}
+	public String printParameters() {
+		if(cmd!= null) {
+			Class<?> c = this.getClass();
+	        Method[] methods = c.getDeclaredMethods();
+	        StringBuffer sb = new StringBuffer();
+	        for(Method m: methods) {
+	       		if(!m.getName().equals("printParameters") && !m.getName().equals("getPrimerPart") && !m.getName().equals("getClass") && !m.getName().equals("hasOptions")) {
+	       			try {
+	       				if(m.invoke(this) != null){
+	       					if(sb.length()>0) {
+	    	            		sb.append("\n");
+	    	            	}
+		       				String str = m.invoke(this).toString();
+		       				String name = m.getName();
+		       				name = name.replace("get","");
+							sb.append(name+": "+str);//+":"+m.invoke(this));
+	       				}
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} 
+					//sb.append(m);//+":"+m.invoke(this));
+	       		}
+	        }
+	        return sb.toString();
+		}
+		return null;
+	}
+	public String getOutput() {
+		return cmd.getOptionValue('o');
+	}
+}
